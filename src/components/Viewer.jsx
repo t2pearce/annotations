30915e2c855def97b39da8f345028763d7c387e2
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-16 16:26:24 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -107,22 +107,21 @@ export default function Viewer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {images.map((group, index) => {
-          <Divider />
-          <h3 key={index}>{group.name}</h3>
-          {group.slides.map((slide, index) => {
-                      <ListItem>
-                      <ButtonToggle
-                        key={index}
-                        active={active === slide}
-                        onClick={() => { setActive(slide);
-                          return previewImage(slide);
-                        }}
-                      >
-                        {slide.name}
-                    </ButtonToggle>
-                    </ListItem>
-        }}
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
       </List>
     </div>
   );
