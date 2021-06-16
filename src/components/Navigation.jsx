aca1ab3ce0f1bad5bf30c516e4f97f3a45eea7c0
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-16 10:56:39 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,33 @@
-
-import React from "react";
+import React, { Component } from 'react';
+import './App.css';
 import { Link, withRouter } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  title:{
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+}));
 
 function Navigation() {
+  const classes = useStyles();
+
   return (
-   <AppBar position="static" centerTitle="true" style={{backgroundColor: '#333333'}}>
-    <Container maxWidth="md">
-      <Toolbar>
-        <Typography variant="h4" color="inherit" align="center">
+  <div className = {classes.root}>
+   <AppBar position="static" style={{backgroundColor: '#333333'}}>
+      <Toolbar className = {classes.title}>
+        <Typography variant="h4" color="inherit">
           Breast Tissue Clinical Study
         </Typography>
       </Toolbar>
-    </Container>
    </AppBar>
+  </div>
   );
 }
 
