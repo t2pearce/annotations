d5aa767849f0986d6769071dc1952b184caf7cad
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-16 11:45:35 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -11,7 +11,6 @@ import Box from '@material-ui/core/Box';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import styled from 'styled-components';
 
