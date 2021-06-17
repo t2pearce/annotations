f8fe86a16183687b5e5d849400d4b9d92ab1d6ce
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-17 10:52:57 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+eimport React from "react";
 import { Link, withRouter } from "react-router-dom";
 import './Home.css';
 import { Button, Box } from "@material-ui/core";
@@ -16,7 +16,7 @@ function Home() {
       <img className="photo" src={logo} />
         <p> </p>
         <p>
-    <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/doctor">Login</Button>
+    <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</Button>
           <p></p>
           <p>Try to open this <a href="viewer">View OpenSeaDragon</a></p>
             </p>
