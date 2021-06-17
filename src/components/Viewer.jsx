ed9669030f3e465163953f8bacd2830d4f4b3020
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-17 17:30:41 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -26,6 +26,7 @@ import { OpenSeaDragonViewer } from './OpenSeaDragonViewer';
 import styled from 'styled-components';
 import PhotoIcon from '@material-ui/icons/Photo';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { ResponsiveContainer } from 'recharts';
 
 const drawerWidth = 200;
 
@@ -254,7 +255,7 @@ export default function Viewer() {
       </Drawer>
         <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
+        <ResponsiveContainer>
            <Grid container spacing={3}>
             <Grid item xs={12} md={12} lg={12}>
               <Paper className={fixedHeightPaper}>
@@ -266,7 +267,7 @@ export default function Viewer() {
               </Paper>
               </Grid>
               </Grid>
-               </Container>
+               </ResponsiveContainer>
       </main>
     </div>
 );
