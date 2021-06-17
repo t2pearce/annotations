cec5a08c013cfdcf8d5c65ef0cf89aa54bfd0f01
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-17 14:22:53 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -96,6 +96,7 @@ const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
+    paddingRight: theme.spacing(4),
   },
   paper: {
     padding: theme.spacing(2),
