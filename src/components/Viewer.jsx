cd07f01f761a2828bc8ef6d19ec3263648261821
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-17 09:01:06 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,139 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import styled from 'styled-components';
-
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
-
-export default function Viewer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-  const [images, setImages] = useState([]);
-  const [manifest, setManifest] = useState();
-  const [active, setActive] = useState();
-  const [title, setTitle] = useState();
+import React from 'react';
+import { Drawer, Divider, IconButton } 
+    from '@material-ui/core';
+import { List, ListItem, ListItemIcon, ListItemText } 
+    from '@material-ui/core';
+import PermContactCalendarIcon from 
+    '@material-ui/icons/PermContactCalendar';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import AccountCircleIcon from 
+    '@material-ui/icons/AccountCircle';
+import { Link } from 'react-router-dom';
   
-  setUserInfo();
-
-  useEffect(() => {
-    getImages();
-  }, []);
-
-  const getImages = async () => {
-    const response = await fetch("api/deepzoom/pictures3.json") //"https://miradortest.z13.web.core.windows.net/pictures3.json"
-    let image = await response.json();
-    console.log('image', image)
-    setImages(image.groups)
-  };
-
-  const previewImage = async (slide) => {
-    setManifest(slide.slide);
-    setTitle(slide.name);
-  };
-
-    async function getUserInfo() {
-        const response = await fetch('/.auth/me');
-        const payload = await response.json();
-        const { clientPrincipal } = payload;
-        return clientPrincipal;
-      }
-
-      async function setUserInfo() {
-        let  clientPrincipal =  await getUserInfo();
-
-        document.getElementById("user").innerHTML = clientPrincipal.userDetails;
-        console.log(clientPrincipal);
-      }
-
-      const Button = styled.button`
-        background-color: black;
-        color: white;
-        font-size: 20px;
-        padding: 10px 60px;
-        border-radius: 0px;
-        margin: 0px 0px;
-        cursor: pointer;
-        &:disabled {
-          color: grey;
-          opacity: 0.7;
-          cursor: default;
-        }
-      `;
-
-      const ButtonToggle = styled(Button)`
-        opacity: 0.6;
-        ${({ active }) =>
-          active &&
-          `
-          opacity: 1;
-        `}
-      `;
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {images.map((group, index) => {
-          <Typography>key={index}>{group.name}</Typography>
-          {group.slides.map((slide, index) => {
-            <ListItem button key={slide.name}>
-            <ListItemText primary={slide.name} />
-              }}
-          </ListItem>
-      </List>
-    </div>
-  );
-
-  return (
-    <div>
-      {['left', 'right', 'top', 'bottom'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
-    </div>
-  );
+const styles = {
+  sideNav: {
+    marginTop: '-60px',
+    zIndex: 3,
+    marginLeft: '0px',
+    position: 'fixed',
+  },
+  link: {
+    color: 'black',
+    textDecoration: 'none',
+  }
+};
+  
+export default class MarerialUIDrawer 
+    extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDrawerOpened: false,
+    };
+  }
+  toggleDrawerStatus = () => {
+    this.setState({
+      isDrawerOpened: true,
+    })
+  }
+  closeDrawer = () => {
+    this.setState({
+      isDrawerOpened: false,
+    })
+  }
+  render() {
+    const { isDrawerOpened } = this.state;
+    return (
+      <div>
+         <div style={styles.sideNav}>
+            <IconButton onClick={this.toggleDrawerStatus}>
+              {!isDrawerOpened ? <ReorderIcon /> : null }
+            </IconButton>
+          </div>
+          <Divider/>
+        <Drawer
+          variant="temporary"
+          open={isDrawerOpened}
+          onClose={this.closeDrawer}
+        >
+          <Link to='/about' style={styles.link}>
+            <List>
+              <ListItem button key='About Us'>
+                <ListItemIcon><AccountCircleIcon/>
+                </ListItemIcon>
+                <ListItemText primary='About Us' />
+              </ListItem>
+            </List>
+          </Link>
+          <Link to='/contact' style={styles.link}>
+          <List>
+            <ListItem button key='Contact Us'>
+              <ListItemIcon><PermContactCalendarIcon/>
+              </ListItemIcon>
+              <ListItemText primary='Contact Us' />
+            </ListItem>
+            </List>
+          </Link>
+        </Drawer>
+      </div>
+    );
+  }
 }
