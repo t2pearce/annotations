fd270086dd875d34824bc268db992e689ca18154
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-21 11:53:08 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -103,6 +103,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column',
+    textAlign: 'center'
   },
   fixedHeight: {
     height: '75vh'
