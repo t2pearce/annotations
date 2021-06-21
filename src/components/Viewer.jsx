603ac3157c9d632202de4fcc75fd7b60352b7b83
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-21 11:40:27 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -256,7 +256,7 @@ export default function Viewer() {
         <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <ResponsiveContainer maxWidth="lg" className={classes.container}>
-           <Grid container spacing={3}>
+           <Grid container spacing={3} alignItems="center">
             <Grid item xs={12} md={12} lg={12}>
               <Paper className={fixedHeightPaper}>
             <Typography align="left">
