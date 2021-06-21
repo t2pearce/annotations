2139a1c247386e8eec21de00f36170d84e17421a
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-21 09:37:11 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import logo from './login.png';
 import { ResponsiveContainer } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
