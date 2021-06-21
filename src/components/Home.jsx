b79972fd95177196b57e3b42bd12d24670c8dcb8
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-21 10:01:31 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,11 +25,11 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     height: "50vh",
     width: "50vw",
-    position: absolute,
-    top: 50%,
-    left: 50%,
-    -ms-transform: translate(-50%, -50%),
-    transform: translate(-50%, -50%)
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    -ms-transform: "translate(-50%, -50%)",
+    transform: "translate(-50%, -50%)"
   },
 }));
 
