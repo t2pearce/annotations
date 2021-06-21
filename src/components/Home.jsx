613ab9abc2c1f0ba21bcd48f3aaf850278c596d3
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-21 10:07:14 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,6 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     top: "50%",
     left: "50%",
-    -ms-transform: "translate(-50%, -50%)",
     transform: "translate(-50%, -50%)"
   },
 }));
