9db0d854f376dd09dd1052fe47564071b4e60474
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-23 11:01:40 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -108,7 +108,8 @@ const useStyles = makeStyles((theme) => ({
   fixedHeight: {
     position: "absolute",
     top: "50%",
-    transform: "translate(0%, -50%)"
+    left: "50%",
+    transform: "translate(-50%, -50%)"
   },
 }));
 
