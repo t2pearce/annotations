53dcb0b7999e78d2de3fea60b3d53b4a364731a0
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-24 13:40:36 -0400

    Update OpenSeaDragonViewer.js

--- src/components/OpenSeaDragonViewer.js
+++ src/components/OpenSeaDragonViewer.js
@@ -1,5 +1,6 @@
 import OpenSeaDragon from "openseadragon";
 import React, { useEffect, useState } from "react";
+import Annotations form "openseadragon-annotations";
 
 
 const OpenSeaDragonViewer = ({ image }) => {
