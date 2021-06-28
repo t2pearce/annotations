b4016b93558c66ea9d8e5cb1805b642b07b44dc9
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-28 09:41:24 -0400

    Update OpenSeaDragonViewer.js

--- src/components/OpenSeaDragonViewer.js
+++ src/components/OpenSeaDragonViewer.js
@@ -1,12 +1,11 @@
 import OpenSeaDragon from "openseadragon";
 import React, { useEffect, useState } from "react";
 import * as Annotorious from '@recogito/annotorious-openseadragon';
-
+import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
 
 const OpenSeaDragonViewer = ({ image }) => {
   const [viewer, setViewer] = useState( null);
   const[anno, setAnno] = useState(null);
-  const [annotations, setAnnotations] = useState([])
 
   useEffect(() => {
     if (image && viewer) {
@@ -16,8 +15,30 @@ const OpenSeaDragonViewer = ({ image }) => {
         InitAnnotations()       
     }  
   }, [image]);
+
+const InitOpenseadragon = () => {
+    viewer && viewer.destroy();
+    
+    const initViewer = OpenSeaDragon({
+        id: "openSeaDragon",
+        prefixUrl: "openseadragon-images/",
+        animationTime: 0.5,
+        blendTime: 0.1,
+        constrainDuringPan: true,
+        maxZoomPixelRatio: 2,
+        minZoomLevel: 1,
+        visibilityRatio: 1,
+        zoomPerScroll: 2
+      })
+    setViewer(initViewer );
+    const config = {};
+    const annotate = Annotorious(initViewer, config);
+    setAnno(annotate)
+  };
   
-  const InitAnnotations = async() => {
+   const [annotations, setAnnotations] = useState([])
+  
+    const InitAnnotations = async() => {
   const storedAnnoatations = getLocalAnnotations
     if (storedAnnoatations) {
         const annotations = parseJSON(storedAnnoatations)
@@ -43,26 +64,6 @@ const OpenSeaDragonViewer = ({ image }) => {
         setLocalAnnotation(newAnnotations)
     });
 }
-
-const InitOpenseadragon = () => {
-    viewer && viewer.destroy();
-    
-    const initViewer = OpenSeaDragon({
-        id: "openSeaDragon",
-        prefixUrl: "openseadragon-images/",
-        animationTime: 0.5,
-        blendTime: 0.1,
-        constrainDuringPan: true,
-        maxZoomPixelRatio: 2,
-        minZoomLevel: 1,
-        visibilityRatio: 1,
-        zoomPerScroll: 2
-      })
-    setViewer(initViewer );
-    const config = {};
-    const annotate = Annotorious(initViewer, config);
-    setAnno(annotate)
-  };
   
   const getLocalAnnotations =  () => {
     return localStorage.getItem(image.source.Image.Url) 
