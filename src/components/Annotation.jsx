8c847905d70eef0ca5610fd8d8f31cbaa7f1fadc
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-28 08:22:02 -0400

    Create Annotation.jsx

---
+++ src/components/Annotation.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from 'react';
+import { Annotorious } from '@recogito/annotorious';
+
+import '@recogito/annotorious/dist/annotorious.min.css';
+
+function App() {
+
+  // Ref to the image DOM element
+  const imgEl = useRef();
+
+  // The current Annotorious instance
+  const [ anno, setAnno ] = useState();
+
+  // Current drawing tool name
+  const [ tool, setTool ] = useState('rect');
+
+  // Init Annotorious when the component
+  // mounts, and keep the current 'anno'
+  // instance in the application state
+  useEffect(() => {
+    let annotorious = null;
+
+    if (imgEl.current) {
+      // Init
+      annotorious = new Annotorious({
+        image: imgEl.current
+      });
+
+      // Attach event handlers here
+      annotorious.on('createAnnotation', annotation => {
+        console.log('created', annotation);
+      });
+
+      annotorious.on('updateAnnotation', (annotation, previous) => {
+        console.log('updated', annotation, previous);
+      });
+
+      annotorious.on('deleteAnnotation', annotation => {
+        console.log('deleted', annotation);
+      });
+    }
+
+    // Keep current Annotorious instance in state
+    setAnno(annotorious);
+
+    // Cleanup: destroy current instance
+    return () => annotorious.destroy();
+  }, []);
+
+  // Toggles current tool + button label
+  const toggleTool = () => {
+    if (tool === 'rect') {
+      setTool('polygon');
+      anno.setDrawingTool('polygon');
+    } else {
+      setTool('rect');
+      anno.setDrawingTool('rect');
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={toggleTool}>
+            { tool === 'rect' ? 'RECTANGLE' : 'POLYGON' }
+        </button>
+      </div>
+
+      <img 
+        ref={imgEl} 
+        src="640px-Hallstatt.jpg" 
+        alt="Hallstatt Town Square" />
+    </div>
+  );
+}
+
+export default App;
