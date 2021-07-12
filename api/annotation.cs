de6d3c05e4aabde9a608b8e37b38118d898f0da7
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-12 13:32:44 -0400

    Update annotation.cs

--- api/annotation.cs
+++ api/annotation.cs
@@ -6,81 +6,92 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
 using System.Security.Claims;
 
 namespace Microsoft.Function
 {
+
+    public class AnnotationItem
+    {
+        [JsonProperty("userId")]
+        public string userId { get; set; }
+        [JsonProperty("id")]
+        public string id { get; set; }
+        //[JsonProperty("annotations")]
+        public object[] AnnotationJson { get; set; }
+    }
+
+
     public static class Annotation
     {
-        public class AnnotationItem
+        [FunctionName("saveAnnotation")]
+        public static  void RunSave(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "annotation/{imageId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "medimages",
+                collectionName: "Annotations",
+                ConnectionStringSetting = "CosmosDBConnection")
+            ] out dynamic document,
+            string imageId,
+            ILogger log)
         {
-            [JsonProperty("userId")]
-            public string userId { get; set; }
-            [JsonProperty("annotation")]
-            public object[] AnnotationJson { get; set; }
+            log.LogInformation($"C# save annotations for {imageId}");
+            document = null;
+
+            // Verify identity
+            ClaimsPrincipal principal = ClientPrincipal.Parse(req);
+            if (!principal.IsInRole("contributor"))
+                return ;
+
+            string userId = principal.Identity.Name;
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var input = JsonConvert.DeserializeObject<AnnotationItem>(requestBody);
+
+            document = new { userId = userId, id = imageId, AnnotationJson = input.AnnotationJson }; //new object[] { requestBody } };
         }
-        
-                [FunctionName("saveAnnotation")]
-                public static async Task<IActionResult> RunSave(
-                    [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "annotation/{userId}")] HttpRequest req,
-                    [CosmosDB(
-                        databaseName: "medimages",
-                        collectionName: "Annotations",
-                        ConnectionStringSetting = "CosmosDBConnection")
-                    ]  DocumentClient client,
-                    ILogger log)
-                {
-                    log.LogInformation("C# HTTP trigger function processed a request.");
-                    
-                    AnnotationItem annotationItem = new AnnotationItem(req.Query["imageId"], JSON.parse(req.Query["annotation"]));
-                    try
-                    {
-                        var response = await client.CreateDocumentAsync(
-                            UriFactory.CreateDocumentUri("medimages", "Annotations", userId),
-                            annotationItem);
 
-                    } catch (Exception e) {
-                        log.LogError($"Cant find Annotation entry for {userId} in cosmosdb");
-                    }
-                    
-                    return new OkObjectResult();
-                }
+        [FunctionName("getAnnotation")]
+        public static async Task<IActionResult> RunGet(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "annotation/{imageId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "medimages",  
+                collectionName: "Profiles",
+                ConnectionStringSetting = "CosmosDBConnection")
+           ]  DocumentClient client,
+            string imageId,
+            ILogger log)
+        {
+            log.LogInformation($"function GetAnnotations {imageId}");
 
-                [FunctionName("getAnnotation")]
-                public static async Task<IActionResult> RunGet(
-                   [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "annotation/{userId}")] HttpRequest req,
-                    [CosmosDB(
-                        databaseName: "medimages",
-                        collectionName: "Annotations",
-                        ConnectionStringSetting = "CosmosDBConnection")
-                    ]  DocumentClient client,
-                   ILogger log)
-                {
-                    log.LogInformation("C# HTTP trigger function processed a request.");
+            // Verify identity
+            ClaimsPrincipal principal = ClientPrincipal.Parse(req);
+            if (!principal.IsInRole("contributor"))
+              return new UnauthorizedResult();
 
-                    ClaimsPrincipal principal = ClientPrincipal.Parse(req);
-                    string userId = principal.Identity.Name;
-                    AnnotationItem annotationItem = null;
-                    try
-                    {
-                        var response = await client.ReadDocumentAsync(
-                            UriFactory.CreateDocumentUri("medimages", "Annotations", userId),
-                            new RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(userId) });
+            string userId = principal.Identity.Name;
+            AnnotationItem annotations = null;
+            try
+            {
+                var response = await client.ReadDocumentAsync<AnnotationItem>(
+                    UriFactory.CreateDocumentUri("medimages", "Annotations", imageId),
+                    new RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(userId) });
 
-                        annotationItem = (AnnotationItem)(dynamic)response.Resource;
-
-                    } catch (Exception e) {
-                        log.LogError($"Cant find Annotation entry for {userId} in cosmosdb");
-                    }
-                    
-                    if (annotationItem == null)
-                    {
-                        //do something   
-                    }
-
-                    return new OkObjectResult(annotationItem.AnnotationJson[0]);
-                }
+                annotations = (AnnotationItem)(dynamic)response;
+                log.LogInformation($"function GetAnnotations invoked {imageId} {userId} {annotations}");
+            }
+            catch (Exception )
+            {
+                log.LogError($"Cant find Annotations entry for {userId}  Image {imageId} in cosmosdb");
+                return new NotFoundResult();
+            }
+            log.LogInformation($"Retrieved annotation {annotations.AnnotationJson}");
+            if ( annotations != null && annotations.AnnotationJson != null && annotations.AnnotationJson.Length > 0)
+              return new OkObjectResult(annotations.AnnotationJson);
+            else
+              return new NotFoundResult();
+        }
     }
 }
