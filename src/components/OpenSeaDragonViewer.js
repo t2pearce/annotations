a3c63e3c0d51b9189617384a28b41a98e5127c44
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-13 15:57:08 -0400

    Update OpenSeaDragonViewer.js

--- src/components/OpenSeaDragonViewer.js
+++ src/components/OpenSeaDragonViewer.js
@@ -37,7 +37,7 @@ const OpenSeaDragonViewer = ({ image }) => {
 
     setViewer(initViewer );
     const config = {formatter: Annotorious.ShapeLabelsFormatter};
-    const annotate = Annotorious(initViewer, config);
+    const annotate = Annotorious.init({initViewer, config});
     setAnno(annotate)
     
     /*Annotorious.SelectionPack(annotate);
