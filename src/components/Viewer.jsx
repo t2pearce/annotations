585f02ac9903176e16b45e5e3130eb84ae51cd9c
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-14 09:39:29 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import './Viewer.css';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
