e12b06966186f40533b85ad677040f85e8adc838
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-15 13:32:20 -0400

    Update OpenSeaDragonViewer.js

--- src/components/OpenSeaDragonViewer.js
+++ src/components/OpenSeaDragonViewer.js
@@ -2,9 +2,72 @@ import OpenSeaDragon,  { parseJSON } from "openseadragon";
 import React, { useEffect, useState } from "react";
 import * as Annotorious from '@recogito/annotorious-openseadragon';
 import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
-import ShapeLabelsFormatter from '@recogito/annotorious-shape-labels';
-import ColorSelectorWidget from './ColorSelector.js';
-import ColorFormatter from './ColorFormatter.js';
+import './ColourSelector.css';
+
+var ColorSelectorWidget = function(args) {
+
+  // 1. Find a current color setting in the annotation, if any
+  var currentColorBody = args.annotation ? 
+    args.annotation.bodies.find(function(b) {
+      return b.purpose == 'highlighting';
+    }) : null;
+
+  // 2. Keep the value in a variable
+  var currentColorValue = currentColorBody ? currentColorBody.value : null;
+
+  // 3. Triggers callbacks on user action
+  var addTag = function(evt) {
+    if (currentColorBody) {
+      args.onUpdateBody(currentColorBody, {
+        type: 'TextualBody',
+        purpose: 'highlighting',
+        value: evt.target.dataset.tag;
+      });
+    } else { 
+      args.onAppendBody({
+        type: 'TextualBody',
+        purpose: 'highlighting',
+        value: evt.target.dataset.tag;
+      });
+    }
+  }
+
+  // 4. This part renders the UI elements
+  var createButton = function(value) {
+    var button = document.createElement('button');
+
+    if (value == currentColorValue)
+      button.className = 'selected';
+
+    button.dataset.tag = value;
+    button.style.backgroundColor = value;
+    button.addEventListener('click', addTag); 
+    return button;
+  }
+
+  var container = document.createElement('div');
+  container.className = 'colorselector-widget';
+  
+  var button1 = createButton('RED');
+  var button2 = createButton('GREEN');
+  var button3 = createButton('BLUE');
+
+  container.appendChild(button1);
+  container.appendChild(button2);
+  container.appendChild(button3);
+
+  return container;
+}
+
+/** A matching formatter that sets the color according to the 'highlighting' body value **/
+var ColorFormatter = function(annotation) {
+  var highlightBody = annotation.bodies.find(function(b) {
+    return b.purpose == 'highlighting';
+  });
+
+  if (highlightBody)
+    return highlightBody.value;
+}
 
 const OpenSeaDragonViewer = ({ image }) => {
   const [viewer, setViewer] = useState( null);
