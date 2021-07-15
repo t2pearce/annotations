6b7f31e746dadb488d10095ac334923d0dc00f2e
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-15 11:46:59 -0400

    Create ColorFormatter.js

---
+++ src/components/ColorFormatter.js
@@ -0,0 +1,9 @@
+/** A matching formatter that sets the color according to the 'highlighting' body value **/
+var ColorFormatter = function(annotation) {
+  var highlightBody = annotation.bodies.find(function(b) {
+    return b.purpose == 'highlighting';
+  });
+
+  if (highlightBody)
+    return highlightBody.value;
+}
