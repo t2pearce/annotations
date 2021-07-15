26a32f1ad03e3c903e72cb9c67bf26fe0549ffc4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-15 13:59:27 -0400

    Delete ColorFormatter.js

--- src/components/ColorFormatter.js
+++
@@ -1,9 +0,0 @@
-/** A matching formatter that sets the color according to the 'highlighting' body value **/
-var ColorFormatter = function(annotation) {
-  var highlightBody = annotation.bodies.find(function(b) {
-    return b.purpose == 'highlighting';
-  });
-
-  if (highlightBody)
-    return highlightBody.value;
-}
