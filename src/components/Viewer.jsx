7e2216e4d4b11fb66e2a14108f0d577e47a10bf1
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-19 14:18:47 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -22,23 +22,111 @@ import { OpenSeaDragonViewer } from './OpenSeaDragonViewer'
 import PhotoIcon from '@material-ui/icons/Photo';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import * as styleRefs from './ViewerStyle.js';
-import { useStyles } from 'react-treat';
 
 const drawerWidth = 200;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(0),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(4),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  fixedHeight: {
+    paddingTop: 4,
+    position: "absolute",
+    top: "55%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  },
+    paperShift: {
+    marginLeft: 100,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+}));
+
 export default function Viewer() {
-  
+
   const [images, setImages] = useState([]);
     const [manifest, setManifest] = useState();
     const [title, setTitle] = useState();
-  const styles = useStyles(styleRefs);
-    
+
     setUserInfo();
 
   useEffect(() => {
     getImages();
   }, []);
-
   const getImages = async () => {
     const response = await fetch("/api/profile", {
                               method: 'GET',
@@ -48,26 +136,23 @@ export default function Viewer() {
     console.log('image', image)
     setImages(image.groups)
   };
-
   const previewImage = async (slide) => {
     setManifest(slide.slide);
     setTitle(slide.name);
   };
-
     async function getUserInfo() {
         const response = await fetch('/.auth/me');
         const payload = await response.json();
         const { clientPrincipal } = payload;
         return clientPrincipal;
       }
-
       async function setUserInfo() {
         let  clientPrincipal =  await getUserInfo();
-
         document.getElementById("user").innerHTML = clientPrincipal.userDetails;
         console.log(clientPrincipal);
       }
-  
+
+  const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -75,23 +160,23 @@ export default function Viewer() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(styles.paper, styles.fixedHeight, open && styles.paperShift);
-  
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight, open && classes.paperShift);
+
   return (
-    <div className={styles.root}>
+    <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(styles.appBar, open && styles.appBarShift)}>
-        <Toolbar className={styles.toolbar}>
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
-            className={clsx(styles.menuButton, open && styles.menuButtonHidden)}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
           >
             <MenuIcon />
           </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={styles.title} align="left">
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title} align="left">
             <b>Breast Tissue Clinical Study</b>
           </Typography>
           <Typography>
@@ -103,11 +188,11 @@ export default function Viewer() {
       <Drawer
         variant="permanent"
         classes={{
-          paper: clsx(styles.drawerPaper, !open && styles.drawerPaperClose),
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
         }}
         open={open}
       >
-        <div className={styles.toolbarIcon}>
+        <div className={classes.toolbarIcon}>
           <IconButton onClick={handleDrawerClose}>
             <ChevronLeftIcon />
           </IconButton>
@@ -147,9 +232,9 @@ export default function Viewer() {
       </div>
 </List>
       </Drawer>
-        <main className={styles.content}>
-        <div className={styles.appBarSpacer} />
-        <Container maxWidth="lg" className={styles.container}>
+        <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
            <Grid container spacing={3} alignItems="center">
             <Grid item xs={12} md={12} lg={12}>
               <Paper className={fixedHeightPaper}>
