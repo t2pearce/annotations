18d994d9244a0e9e020cf3d38e7de83b38bb89a4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-19 13:36:51 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -4,7 +4,6 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -14,19 +13,14 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { OpenSeaDragonViewer } from './OpenSeaDragonViewer';
-import styled from 'styled-components';
+import { OpenSeaDragonViewer } from './OpenSeaDragonViewer'
 import PhotoIcon from '@material-ui/icons/Photo';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import { ResponsiveContainer } from 'recharts';
 
 const drawerWidth = 200;
 
@@ -125,7 +119,6 @@ export default function Viewer() {
   
   const [images, setImages] = useState([]);
     const [manifest, setManifest] = useState();
-    const [active, setActive] = useState();
     const [title, setTitle] = useState();
     
     setUserInfo();
