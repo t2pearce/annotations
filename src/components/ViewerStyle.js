00abd1ae486b76423dc255924aa174ff252b3294
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-19 13:16:00 -0400

    Update and rename Viewer.css to ViewerStyle.js

--- src/components/ViewerStyle.js
+++ src/components/ViewerStyle.js
@@ -1,50 +1,45 @@
-  const drawerWidth = 200;
-.root: {
-    display: flex;
-  }
+import { makeStyles } from '@material-ui/core/styles';
+const drawerWidth = 200;
 
-  .toolbar: {
-    paddingRight: 24; // keep right padding when drawer closed
-  }
-
-  .toolbarIcon: {
-    display: flex;
-    alignItems: center;
-    justifyContent: flex-end;
-    padding: 0 8px;
-    ...theme.mixins.toolbar;
-  }
-
-  .appBar: {
-    zIndex: theme.zIndex.drawer + 1;
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-  }
-
-  .appBarShift: {
-    marginLeft: drawerWidth;
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-  }
-
-  .menuButton: {
+  },
+  menuButton: {
     marginRight: 36,
-  }
-
-  .menuButtonHidden: {
+  },
+  menuButtonHidden: {
     display: 'none',
-  }
-
-  .title: {
+  },
+  title: {
     flexGrow: 1,
-  }
-
-  .drawerPaper: {
+  },
+  drawerPaper: {
     position: 'relative',
     whiteSpace: 'nowrap',
     width: drawerWidth,
@@ -52,9 +47,8 @@
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-  }
-
-  .drawerPaperClose: {
+  },
+  drawerPaperClose: {
     overflowX: 'hidden',
     transition: theme.transitions.create('width', {
       easing: theme.transitions.easing.sharp,
@@ -64,9 +58,8 @@
     [theme.breakpoints.up('sm')]: {
       width: theme.spacing(0),
     },
-  }
-
-  .appBarSpacer: theme.mixins.toolbar,
+  },
+  appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     height: '100vh',
@@ -96,4 +89,5 @@
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-  }
+  },
+}));
