bdb446c6f35913f230a8b2b46c3025b5d5f0a5a4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-21 15:39:45 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -99,22 +99,19 @@ export default function Viewer2() {
       </AppBar>
         <main className={clsx(classes.content, {
                           [classes.contentShift]:open, })}>
-              <div className={classes.appBarSpacer} />
-           <Container maxWidth="lg" className={clsx(classes.container, {
-                                                     [classes.contentShift]:open, })}>
-           <Grid container spacing={3} alignItems="center">
-            <Grid item xs={12} md={12} lg={12}>
-              <Paper className={clsx(fixedHeightPaper, {
-                       [classes.contentShift]:open, })}>
-            <Typography align="left">
-               Image: <b>{title}</b>
-                <p></p>
-             </Typography>
-            <OpenSeaDragonViewer2 image={manifest} />
-              </Paper>
-              </Grid>
-              </Grid>
-               </Container>
+             <div className={classes.appBarSpacer} />
+           <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
       </main>
       <Drawer
         className={classes.drawer}
