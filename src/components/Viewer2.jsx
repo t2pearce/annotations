bcaeb2983e827ba427a35196e38addb5a47ed2c4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-21 10:46:58 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -145,10 +145,6 @@ export default function Viewer2() {
            <Grid container spacing={3} alignItems="center">
             <Grid item xs={12} md={12} lg={12}>
               <Paper className={fixedHeightPaper}>
-                <Typography align="left">
-                  Hold the <b>[SHIFT]</b> key while clicking and dragging the mouse to create a new annotation.
-                  <p></p>
-                </Typography>
             <Typography align="left">
                Image: <b>{title}</b>
                 <p></p>
