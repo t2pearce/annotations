e438f9ae6a7c82264530d6e1d31c3851c232d75c
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-21 16:18:59 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -100,7 +100,7 @@ export default function Viewer2() {
         <main className={clsx(classes.content, {
                           [classes.contentShift]:open, })}>
              <div className={classes.drawerHeader} />
-           <Paper variant="outlined" style={{width: "75vw"}}>
+           <Paper variant="outlined" align ="center" style={{width: "75vw"}}>
              <OpenSeaDragonViewer2 image={manifest} />
                </Paper>
 </main>
