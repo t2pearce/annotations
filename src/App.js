d5bab349edb6efe4ae8e1f6d63d0b65abb97e963
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-22 10:03:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
           <Route path="/" exact component={() => <Home />} />
           <Route path="/viewer" exact component={() => <Viewer />} />
           <Route path="/viewer2" exact component={() => <Viewer2 />} />
+          <Route path="/doctor" exact component={() => <Doctor />} />
         </Switch>
       </Router>
     </div>
