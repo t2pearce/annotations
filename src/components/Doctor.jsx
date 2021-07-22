a977ae9ca60658139b8437ecdf432ef5df2b89d6
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-22 10:01:49 -0400

    Create Doctor.jsx

---
+++ src/components/Doctor.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import './Home.css';
+import { Button, Grid, Paper } from "@material-ui/core";
+import logo from './login.png';
+import { ResponsiveContainer } from 'recharts';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  fixedHeight: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  }
+}));
+
+function Doctor() {
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  return (
+    <ResponsiveContainer maxWidth="lg" className={classes.container}>
+    <Grid container spacing={3} alignItems="center">
+    <Grid item xs={12} md={12} lg={12}>
+      <Paper className={fixedHeightPaper}>
+        <Typography align="center">
+      <p> [INSERT TEXT HERE] </p>
+      <p></p>
+        <p>
+    <Button variant="contained" href="/viewer">Operator</Button>
+          <p></p>
+          <Button variant="contained" href="/viewer">Clinician</Button>
+            </p>
+    </Typography>
+    </Paper>
+    </Grid>
+    </Grid>
+    </ResponsiveContainer>
+  );
+}
+
+export default Doctor;
