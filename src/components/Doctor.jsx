97f5cc9b7285c8ee61ad46545a18f86ac7fa6101
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-22 12:23:30 -0400

    Update Doctor.jsx

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -6,26 +6,49 @@ import { ResponsiveContainer } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Typography from '@material-ui/core/Typography';
-
-export default function Doctor() {
-  async function getUserInfo() {
-        const response = await fetch('/.auth/me');
-        const payload = await response.json();
-        const { clientPrincipal } = payload;
-        return clientPrincipal;
-      }
-  
-  const redirect = null;
-  
-  if (clientPrincipal.userRoles == 'operator') {
-    redirect = "/viewer";
-  } else if (clientPrincipal.userRoles == 'operator') {
-    redirect = "viewer2";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  fixedHeight: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
   }
-    
+}));
+function Doctor() {
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return (
-    <Switch>
-    <RedirectWithStatus status={301} from="/doctor" to="{redirect}" />
-    </Switch>
-   );
+    <ResponsiveContainer maxWidth="lg" className={classes.container}>
+    <Grid container spacing={3} alignItems="center">
+    <Grid item xs={12} md={12} lg={12}>
+      <Paper className={fixedHeightPaper}>
+        <Typography align="center">
+      <p> [INSERT TEXT HERE] </p>
+      <p></p>
+        <p>
+    <Button variant="contained" href="/viewer">Operator</Button>
+          <p></p>
+          <Button variant="contained" href="/viewer2">Clinician</Button>
+            </p>
+    </Typography>
+    </Paper>
+    </Grid>
+    </Grid>
+    </ResponsiveContainer>
+  );
 }
+export default Doctor;
