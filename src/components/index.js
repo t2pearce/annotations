e4e1306fc08202161ad1b9a1bce60feba741f954
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-26 14:00:34 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { default as Home } from "./Home";
 export { default as Viewer } from "./Viewer";
 export { default as Viewer2 } from "./Viewer2";
 export { default as Doctor } from "./Doctor";
-export { default as Questions } from "./Questions";
