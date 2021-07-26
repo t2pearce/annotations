29f5896ccc2cb3a1426358fbe5e24de06993618d
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-26 15:02:01 -0400

    Update Questions.jsx

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -12,48 +12,24 @@ import React, {useState} from 'react';
 import './Questions.css';
 
 export default function Questions() {
-  const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
+
+  useEffect(() => {
+	getQuestions();
+  }, []);
+	
+  const getQuestions = async () => {
+    const response = await fetch("/api/questions", {
+                              method: 'GET',
+                              credentials: 'include',
+                              headers: {'Access-Control-Allow-Credentials': 'true'}}); 
+    let questionList = await response.json();
+    setQuestions(image.questionList)
+  };
   
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showProgress, setShowProgress] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [questions, setQuestions] = useState();
   
   const handleAnswerOptionClick = (isCorrect) => {
     if(isCorrect) {
