d7cd4267689fb9e253091f33e2a87f09bb20f5f6
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-27 15:13:23 -0400

    Update ColorSelectorWidget.js

--- src/components/ColorSelectorWidget.js
+++ src/components/ColorSelectorWidget.js
@@ -1,57 +1,28 @@
 import './ColorSelector.css';
+import React from 'react';
 
-var ColorSelectorWidget = function(args) {
+const HelloWorldWidget = props => {
+  // We'll be using 'highlighting' as body purpose for 
+  // this type of widget
+  const currentHighlight = props.annotation ? 
+    props.annotation.bodies.find(b => b.purpose === 'highlighting') : null;
 
-  // 1. Find a current color setting in the annotation, if any
-  var currentColorBody = args.annotation ? 
-    args.annotation.bodies.find(function(b) {
-      return b.purpose == 'highlighting';
-    }) : null;
-
-  // 2. Keep the value in a variable
-  var currentColorValue = currentColorBody ? currentColorBody.value : null;
-
-  // 3. Triggers callbacks on user action
-  var addTag = function(evt) {
-    if (currentColorBody) {
-      args.onUpdateBody(currentColorBody, {
-        type: 'TextualBody',
-        purpose: 'highlighting',
-        value: evt.target.dataset.tag,
-      });
-    } else { 
-      args.onAppendBody({
-        type: 'TextualBody',
-        purpose: 'highlighting',
-        value: evt.target.dataset.tag,
-      });
-    }
+  // This function handles body updates as the user presses buttons
+  const setHighlightBody = value => () => {
+    props.onUpsertBody(currentHighlight, { value, purpose: 'highlighting' });
   }
 
-  // 4. This part renders the UI elements
-  var createButton = function(value) {
-    var button = document.createElement('button');
+  return (
+    <div className="helloworld-widget">
+      { [ 'red', 'green', 'blue' ].map(color => 
+        <button 
+          key={color}    
+          className={currentHighlight?.value === color ? 'selected' : null}
+          style={{ backgroundColor: color }}
+          onClick={setHighlightBody(color)} />
+      )}
+    </div>
+  )
 
-    if (value == currentColorValue)
-      button.className = 'selected';
-
-    button.dataset.tag = value;
-    button.style.backgroundColor = value;
-    button.addEventListener('click', addTag); 
-    return button;
-  }
-
-  var container = document.createElement('div');
-  container.className = 'colorselector-widget';
-  
-  var button1 = createButton('RED');
-  var button2 = createButton('GREEN');
-  var button3 = createButton('BLUE');
-
-  container.appendChild(button1);
-  container.appendChild(button2);
-  container.appendChild(button3);
-
-  return container;
 }
 export default ColorSelectorWidget;
