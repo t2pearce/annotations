f0327f581644e4d1fe1c7d7c32542aaa8acff43b
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-27 15:31:32 -0400

    Create ColorSelectorWidget.js

--- src/components/ColorSelectorWidget.js
+++ src/components/ColorSelectorWidget.js
@@ -1,28 +1,53 @@
 import './ColorSelector.css';
 import React from 'react';
 
-const ColorSelectorWidget = props => {
-  // We'll be using 'highlighting' as body purpose for 
-  // this type of widget
-  const currentHighlight = props.annotation ? 
-    props.annotation.bodies.find(b => b.purpose === 'highlighting') : null;
+var ColorSelectorWidget = function(args) {
 
-  // This function handles body updates as the user presses buttons
-  const setHighlightBody = value => () => {
-    props.onUpsertBody(currentHighlight, { value, purpose: 'highlighting' });
+  var currentColorBody = args.annotation ? args.annotation.bodies.find(function(b) {
+    return b.purpose == 'highlighting';
+  }) : null;
+
+  var currentColorValue = currentColorBody ? currentColorBody.value : null;
+
+  var addTag = function(evt) {
+    if (currentColorBody) {
+      args.onUpdateBody(currentColorBody, {
+        type: 'TextualBody',
+        purpose: 'highlighting',
+        value: evt.target.dataset.tag;
+      });
+    } else { 
+      args.onAppendBody({
+        type: 'TextualBody',
+        purpose: 'highlighting',
+        value: evt.target.dataset.tag;
+      });
+    }
   }
 
-  return (
-    <div className="helloworld-widget">
-      { [ 'red', 'green', 'blue' ].map(color => 
-        <button 
-          key={color}    
-          className={currentHighlight?.value === color ? 'selected' : null}
-          style={{ backgroundColor: color }}
-          onClick={setHighlightBody(color)} />
-      )}
-    </div>
-  )
+  var createButton = function(value) {
+    var button = document.createElement('button');
 
+    if (value == currentColorValue)
+      button.className = 'selected';
+
+    button.dataset.tag = value;
+    button.style.backgroundColor = value;
+    button.addEventListener('click', addTag); 
+    return button;
+  }
+
+  var container = document.createElement('div');
+  container.className = 'helloworld-plugin';
+  
+  var button1 = createButton('RED');
+  var button2 = createButton('GREEN');
+  var button3 = createButton('BLUE');
+
+  container.appendChild(button1);
+  container.appendChild(button2);
+  container.appendChild(button3);
+
+  return container;
 }
 export default ColorSelectorWidget;
