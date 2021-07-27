c38f0d9752adbaa5e6b0a44ada3ed91c93b6cf6e
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-27 15:03:19 -0400

    Update ColorFormatter.js

--- src/components/ColorFormatter.js
+++ src/components/ColorFormatter.js
@@ -1,3 +1,4 @@
+import './ColorSlector.css';
 /** A matching formatter that sets the color according to the 'highlighting' body value **/
 var ColorFormatter = function(annotation) {
   var highlightBody = annotation.bodies.find(function(b) {
