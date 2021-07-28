6ee5901346c10d7a74374d1bfbaeeb76ef2af09b
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-07-28 14:22:11 -0400

    Update Doctor.jsx

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -71,7 +71,7 @@ function Doctor() {
     }
   }
 
-  return (/*
+  return (
     <ResponsiveContainer maxWidth="lg" className={classes.container}>
     <Grid container spacing={3} alignItems="center">
     <Grid item xs={12} md={12} lg={12}>
@@ -88,7 +88,7 @@ function Doctor() {
     </Paper>
     </Grid>
     </Grid>
-    </ResponsiveContainer> */
+    </ResponsiveContainer>
   );
 }
 export default Doctor;
