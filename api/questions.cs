ebb47b2a2e49739bed688e9e1cf28562f01ea868
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-28 15:21:38 -0400

    Update questions.cs

--- api/questions.cs
+++ api/questions.cs
@@ -18,49 +18,50 @@ namespace Microsoft.Function
         {
             [JsonProperty("imageid")]
             public string ImageId { get; set; }
-            [JsonProperty("QuestionsJson")]
+            //[JsonProperty("QuestionsJson")]
             public object[] QuestionsJson { get; set; }
         }
 
-
-        [FunctionName("getQuestions")]
-        public static async Task<IActionResult> RunGet(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "questions/{imageId}")] HttpRequest req,
-            [CosmosDB(
-                databaseName: "medimages",
-                collectionName: "Questions",
-                ConnectionStringSetting = "CosmosDBConnection")
-            ]  DocumentClient client,
-            string imageId,
-            ILogger log)
+        public static class Quuestions 
         {
-            log.LogInformation($"function GetQuestions {imageId}");
-            
-            // Verify identity
-            ClaimsPrincipal principal = ClientPrincipal.Parse(req);
-            if (!principal.IsInRole("contributor"))
-                return new UnauthorizedResult();
-            
-            string userId = principal.Identity.Name;
-            QuestionsItem questions = null;
-            try
+            [FunctionName("getQuestions")]
+            public static async Task<IActionResult> RunGet(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "questions/{imageId}")] HttpRequest req,
+                [CosmosDB(
+                    databaseName: "medimages",
+                    collectionName: "Questions",
+                    ConnectionStringSetting = "CosmosDBConnection")
+                ]  DocumentClient client,
+                string imageId,
+                ILogger log)
             {
-                var response = await client.ReadDocumentAsync(
-                    UriFactory.CreateDocumentUri("medimages", "Questions", imageId),
-                    new RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(imageId) });
+                log.LogInformation($"function GetQuestions {imageId}");
 
-                questions = (QuestionsItem)(dynamic)response.Resource;
-                log.LogInformation($"function GetQuestions invoked {imageId} {questions}");
+                // Verify identity
+                ClaimsPrincipal principal = ClientPrincipal.Parse(req);
+                if (!principal.IsInRole("contributor"))
+                    return new UnauthorizedResult();
 
-            } catch (Exception ) {
-                log.LogError($"Cant find Questions entry for {imageId} in cosmosdb");
-                return new NotFoundResult();
+                string userId = principal.Identity.Name;
+                QuestionsItem questions = null;
+                try
+                {
+                    var response = await client.ReadDocumentAsync(
+                        UriFactory.CreateDocumentUri("medimages", "Questions", imageId),
+                        new RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(imageId) });
+
+                    questions = (QuestionsItem)(dynamic)response.Resource;
+                    log.LogInformation($"function GetQuestions invoked {imageId} {questions}");
+
+                } catch (Exception ) {
+                    log.LogError($"Cant find Questions entry for {imageId} in cosmosdb");
+                    return new NotFoundResult();
+                }
+                log.LogInformation($"Retrieved questions {questions.QuestionsJson}");
+                if ( questions != null && questions.QuestionsJson != null && questions.QuestionsJson.Length > 0)
+                  return new OkObjectResult(questions.QuestionsJson);
+                else
+                  return new NotFoundResult();
             }
-            log.LogInformation($"Retrieved questions {questions.QuestionsJson}");
-            if ( questions != null && questions.QuestionsJson != null && questions.QuestionsJson.Length > 0)
-              return new OkObjectResult(questions.QuestionsJson);
-            else
-              return new NotFoundResult();
         }
-    
 }
