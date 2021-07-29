7335ecde5a3ca8ae2dd0d003de30f43febbce89f
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-07-29 09:50:34 -0400

    Update Doctor.jsx

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -22,7 +22,7 @@ class Doctor extends React.Component {
     // fetch the project name, once it retrieves resolve the promise and update the state. 
     this.getRedirectLink().then(result => this.setState({
       redirectLink: result
-    }, function() console.log(this.state)))
+    }))
   }
 
   async getUserInfo() {
