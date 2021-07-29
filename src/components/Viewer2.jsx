46674cf89a50452b28447ff3a1e39d82d22bda46
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-07-29 10:41:14 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -39,41 +39,41 @@ import './Questions.css';
 
 export default function Viewer2() {
 
-const "questionList": [
+const questionList: [
         {
-            "questionText": "What is the capital of France?",
-            "answerOptions": [
-                {"answerText": "New York"},
-                {"answerText": "London"},
-                {"answerText": "Paris"},
-                {"answerText": "Dublin"}
+            questionText: "What is the capital of France?",
+            answerOptions: [
+                {answerText: "New York"},
+                {answerText: "London"},
+                {answerText: "Paris"},
+                {answerText: "Dublin"}
             ]
         },
         {
-            "questionText": "Who is CEO of Tesla?",
-            "answerOptions": [
-                {"answerText": "Jeff Bezos"},
-                {"answerText": "Elon Musk"},
-                {"answerText": "Bill Gates"},
-                {"answerText": "Tony Stark"}
+            questionText: "Who is CEO of Tesla?",
+            answerOptions: [
+                {answerText: "Jeff Bezos"},
+                {answerText: "Elon Musk"},
+                {answerText: "Bill Gates"},
+                {answerText: "Tony Stark"}
             ]
         },
         {
-            "questionText": "The iPhone was created by which company?",
-            "answerOptions": [
-                {"answerText": "Apple"},
-                {"answerText": "Intel"},
-                {"answerText": "Amazon"},
-                {"answerText": "Microsoft"}
+            questionText: "The iPhone was created by which company?",
+            answerOptions: [
+                {answerText: "Apple"},
+                {answerText: "Intel"},
+                {answerText: "Amazon"},
+                {answerText: "Microsoft"}
             ]
         },
         {
-            "questionText": "How many Harry Potter books are there?",
-            "answerOptions": [
-                {"answerText": "1"},
-                {"answerText": "4"},
-                {"answerText": "6"},
-                {"answerText": "7"}
+            questionText: "How many Harry Potter books are there?",
+            answerOptions: [
+                {answerText: "1"},
+                {answerText: "4"},
+                {answerText: "6"},
+                {answerText: "7"}
             ]
         }
     ],	
