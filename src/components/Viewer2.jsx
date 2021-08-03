1e7ca4de36d1480957e0d581f492639a54311486
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-03 11:40:58 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -275,7 +275,25 @@ else {
           
 <List>
 	<div className='app'>
-	
+			{showScore ? (
+			 <div className='question-section'>
+     			<button onClick={handleNext} variant="contained">Next Image</ button>
+			</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questionList.length}
+						</div>
+						<div className='question-text'>{questionList[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questionList[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.answerText)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
 		</div>	  
        
       
