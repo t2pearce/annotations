0bd873a2049cb9b2d37fc2ffadbf032316dd2711
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-04 14:47:18 -0400

    Update OpenSeaDragonViewer.js

--- src/components/OpenSeaDragonViewer.js
+++ src/components/OpenSeaDragonViewer.js
@@ -19,6 +19,7 @@ const OpenSeaDragonViewer = ({ image }) => {
     }
     if (image && anno) {
       //anno.destroy();
+      anno.clearAnnotations();
       InitAnnotations();
     }
   }, [image]);
