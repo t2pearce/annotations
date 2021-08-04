0c6d764bb4dfeb4aab866fd9f75e3fec2faaf6a9
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-04 09:54:43 -0400

    Update questions.cs

--- api/questions.cs
+++ api/questions.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Function
             string userId = principal.Identity.Name;
 
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-            var input = JsonConvert.SerializeObject(requestBody);
+            //var input = JsonConvert.SerializeObject(requestBody);
 
             document = new { id = userId, imageId = imageId, AnswersJson = input }; //new object[] { requestBody } };
         }
