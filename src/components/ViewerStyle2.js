7616ab8c28c56b7123423dec2d113e889511fc50
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-05 11:02:23 -0400

    Update ViewerStyle2.js

--- src/components/ViewerStyle2.js
+++ src/components/ViewerStyle2.js
@@ -67,7 +67,7 @@ export const useStyles = makeStyles((theme) => ({
     transform: "translate(-50%, -50%)"
   },
   paperShift: {
-    marginLeft: -100,
+    marginRight: 150,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
