74994a2d48154f0426e458852ed54480c01369b0
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-05 12:45:19 -0400

    Update ViewerStyle2.js

--- src/components/ViewerStyle2.js
+++ src/components/ViewerStyle2.js
@@ -68,7 +68,7 @@ export const useStyles = makeStyles((theme) => ({
     paddingTop: 4,
     position: "absolute",
     top: "55%",
-    left: "25%",
+    //left: "25%",
     transform: "translate(-50%, -50%)"
   },
   paperShift: {
