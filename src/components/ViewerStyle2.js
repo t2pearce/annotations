986107b259d89fcaa7e96d847ec8859ca95d10a1
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-05 11:18:55 -0400

    Update ViewerStyle2.js

--- src/components/ViewerStyle2.js
+++ src/components/ViewerStyle2.js
@@ -13,19 +13,23 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
   appBar: {
+    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
+    marginRight: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.easeOut,
+      easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    marginRight: drawerWidth,
   },
   title: {
     flexGrow: 1,
