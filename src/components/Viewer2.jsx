aa3b061d24d17364c808a2e0188988f6b65d16ce
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-05 16:06:40 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -278,7 +278,7 @@ const saveRemoteAnswers =  (newAnswers) => {
                  <Grid item xs={12} md={12} lg={12}>
                    <Paper className={fixedHeightPaper}>
 			   <Typography variant="h6" align="left">
-				 <p></p> <b>Image: </b> {index} <p></p>
+				 <p></p> <b>Image {index} </b> <p></p>
 				</Typography>
                      <OpenSeaDragonViewer2 image={manifest} />
                    </Paper>
