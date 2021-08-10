8bbb9bf34515c3e8d7b8583b0026c3286a1d0682
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-10 14:05:59 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -152,8 +152,8 @@ export default function Viewer2() {
     console.log('slides', image.groups[0].slides)
     setImages(image.groups[0].slides)
     //setManifest(image.groups[0].slides[0].slide)
-    setImageId(image.groups[0].slides[0].slide.source.Image.Url)
-    console.log('IMAGEID', image.groups[0].slides[0].slide.source.Image.Url)
+    setImageId(image.groups[0].slides[index].slide.source.Image.Url)
+    console.log('IMAGEID', image.groups[0].slides[index].slide.source.Image.Url)
     //getQuestions(image.groups[0].slides[0].slide.source.Image.Url)
   };
 	
