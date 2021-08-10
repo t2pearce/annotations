b8392be2c3306369280ade1b852dcf4d42728fa7
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-10 15:26:56 -0400

    Update Viewer2.jsx

--- src/components/Viewer2.jsx
+++ src/components/Viewer2.jsx
@@ -139,9 +139,6 @@ export default function Viewer2() {
     }
 	
   const getImages = async () => {
-	  if(images) {
-		  return
-	  }
     const response = await fetch("/api/profile", {
                               method: 'GET',
                               credentials: 'include',
@@ -152,7 +149,7 @@ export default function Viewer2() {
     console.log('slides', image.groups[0].slides)
     setImages(image.groups[0].slides)
     //setManifest(image.groups[0].slides[0].slide)
-    setImageId(image.groups[0].slides[index].slide.source.Image.Url)
+    setImageId(image.groups[0].slides[imageindexRef].slide.source.Image.Url)
     console.log('IMAGEID', image.groups[0].slides[imageindexRef].slide.source.Image.Url)
     //getQuestions(image.groups[0].slides[0].slide.source.Image.Url)
   };
