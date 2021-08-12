661f7afca7e29ed57090d62e9d0a86aea7ea01de
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-12 14:43:30 -0400

    Update ColorFormatter.js

--- src/components/ColorFormatter.js
+++ src/components/ColorFormatter.js
@@ -7,23 +7,8 @@ var ColorFormatter = function(annotation) {
 
         const firstTag = bodies.find(b => b.purpose == 'tagging');
   
-      const firstComment = bodies.find(b => b.purpose == 'commenting');
-        
-        if (firstComment || firstTag) {
-          const foreignObject = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
-
-          // Fill annotation dimensions
-          foreignObject.setAttribute('width', '100%');
-          foreignObject.setAttribute('height', '100%');
-
-          foreignObject.innerHTML = `
-            <div xmlns="http://www.w3.org/1999/xhtml" class="a9s-shape-label">
-              ${firstComment.value}
-            </div>`;
-
-          return 
-            firstTag.value;
+      if(firstTag) 
+         return firstTag.value;
      
-        }
 }
 export default ColorFormatter;
