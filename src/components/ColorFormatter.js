8e9c8acb0912e1b1f208c4e77a42da7354d5918d
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-12 13:42:52 -0400

    Update ColorFormatter.js

--- src/components/ColorFormatter.js
+++ src/components/ColorFormatter.js
@@ -1,12 +1,31 @@
 import './ColorSelector.css';
+import './ShabeLabels.css';
 /** A matching formatter that sets the color according to the 'highlighting' body value **/
 var ColorFormatter = function(annotation) {
   const bodies = Array.isArray(annotation.body) ?
           annotation.body : [ annotation.body ];
 
         const firstTag = bodies.find(b => b.purpose == 'tagging');
+  
+      const firstComment = bodies.find(b => b.purpose == 'commenting');
+        
+        if (firstComment || firstTag) {
+          const foreignObject = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
 
-  if (firstTag)
-    return firstTag.value;
+          // Fill annotation dimensions
+          foreignObject.setAttribute('width', '100%');
+          foreignObject.setAttribute('height', '100%');
+
+          foreignObject.innerHTML = `
+            <div xmlns="http://www.w3.org/1999/xhtml" class="a9s-shape-label">
+              ${firstComment.value}
+            </div>`;
+
+          return {
+            firstTag.value;
+            element: foreignObject,
+            className: firstComment.value
+          };
+        }
 }
 export default ColorFormatter;
