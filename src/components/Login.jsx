7f6880a103f0820bbcee020a622a7b546e850a66
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-16 10:28:24 -0400

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -46,7 +46,7 @@ function Login() {
       <img className="photo" src={logo} />
         <p> </p>
         <p>
-    <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://black-flower-0f7508210.azurestaticapps.net/doctor">Login</Button>
+    <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://black-flower-0f7508210.azurestaticapps.net/redirectpage">Login</Button>
           <p></p>
           <p>Try to open this <a href="/viewer2">View OpenSeaDragon</a></p>
             </p>
