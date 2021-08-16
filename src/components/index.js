5ebf03a917b4a5a40b795cf7347c44d9b3e1ebf4
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-08-16 10:18:57 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Login } from "./Login";
 export { default as OperatorViewer } from "./OperatorViewer";
 export { default as ClinicianViewer } from "./ClinicianViewer";
-export { default as Redirect } from "./Redirect";
+export { default as RedirectPage } from "./RedirectPage";
