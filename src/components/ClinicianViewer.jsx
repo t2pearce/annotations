d1c253c69cc73a92bc3da69cc43d95dab130ba12
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-16 21:52:41 -0400

    Update ClinicianViewer.jsx

--- src/components/ClinicianViewer.jsx
+++ src/components/ClinicianViewer.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles, useTheme} from '@material-ui/core/styles';
 import { CssBaseline, Drawer, AppbBar, Toolbar, List, Typography, IconButton, Container, Grid, Paper } from '@material-ui/core';
 import { FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel, FormLabel, Button } from '@material-ui/core';
-import { MenuIcon, ChevronLeftIcon, ChevronRightIcon  } from '@material-ui/icons';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { ClinicianOSDViewer } from './ClinicianOSDViewer'
 import {useStyles} from './ClinicianViewerStyle.js';
 import './Questions.css';
