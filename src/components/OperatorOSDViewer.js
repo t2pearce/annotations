c8568eed44480785cea69be90b122484e340beaa
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-19 10:32:12 -0400

    Update OperatorOSDViewer.js

--- src/components/OperatorOSDViewer.js
+++ src/components/OperatorOSDViewer.js
@@ -4,7 +4,7 @@ import * as Annotorious from '@recogito/annotorious-openseadragon';
 import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
 import ShapeLabelsFormatter from './ShapeLabelsFormatter.js';
 import ColorFormatter from './ColorFormatter.js';
-import HelloWorldWidget from './ColourWidget.jsx';
+import HelloWorldWidget from './ColourWidget.js';
 
 
 const OperatorOSDViewer = ({ image }) => {
